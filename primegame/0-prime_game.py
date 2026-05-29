11f299f3303dfd94438379ecbb12ed83889394c1
bassem-krayem <bassem.krayem09@gmail.com>
2026-05-29 03:36:31 +0100

    feature: added prime game project

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,63 +1,66 @@
 #!/usr/bin/python3
-""" Prime Game """
-
-
-def isprime(n):
-    """ n: number to check if it is prime"""
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-def delete_numbers(n, nums):
-    """ delete numbers - assign zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
+"""
+Module for the Prime Game.
+Determines the winner after x rounds of picking primes and their multiples.
+"""
 
 
 def isWinner(x, nums):
-    """ where x is the number of rounds and nums is an array of n
-        Return: name of the player that won the most rounds
-        Iriaf the winner cannot be determined, return None
-        You can assume n and x will not be larger than 10000
     """
-    nums.sort()
-    winner = False
-    Maria = 0
-    Ben = 0
-    for game in range(x):
-        # print("game# ", game+1)
-        nums2 = list(range(1, nums[game] + 1))
-        # print("nums: ", nums2)
-        turn = 0
-        while True:
-            """
-            # uncomment to monitor turns
-            if turn % 2 != 0:
-                print("Ben turn ")
-            else:
-                print("Maria turn ")
-            """
-            change = False
-            for i, n in enumerate(nums2):
-                # print("n: ", n, "i: ", i)
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            # print("movement: ", nums2)
-            if change is False:
-                break
-        if turn % 2 != 0:
-            Maria += 1
-        else:
-            Ben += 1
-        # print("Maria: {}, Ben: {}".format(Maria, Ben))
-    if Maria == Ben:
+    Determines the winner of the Prime Game.
+
+    Args:
+        x (int): Number of rounds.
+        nums (list): Array of n values for each round.
+
+    Returns:
+        str: Name of the player who won the most rounds ("Maria" or "Ben").
+        None: If the winner cannot be determined (tie) or invalid inputs.
+    """
+    if not nums or x < 1:
         return None
-    if Maria > Ben:
+
+    # Find the maximum value of n to dynamically scale our sieve limit
+    max_n = max(nums)
+
+    # 1. Sieve of Eratosthenes to precompute primes up to max_n
+    # True indicates a prime, False indicates non-prime
+    sieve = [True] * (max_n + 1)
+    if max_n >= 0:
+        sieve[0] = False
+    if max_n >= 1:
+        sieve[1] = False
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_n + 1, i):
+                sieve[j] = False
+
+    # 2. Cumulative count of primes up to each index i
+    # This lets us know exactly how many prime turns occur in O(1) time
+    prime_counts = [0] * (max_n + 1)
+    count = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            count += 1
+        prime_counts[i] = count
+
+    # 3. Simulate rounds using the precomputed prime counts
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        # Get the number of primes up to n
+        primes_available = prime_counts[n]
+        # If odd number of primes, Maria wins. If even, Ben wins.
+        if primes_available % 2 != 0:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # 4. Determine final match winner
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    return None
